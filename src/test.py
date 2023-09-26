7f1300876849db5352bd4bbd8620af5040404303
ronik-v <andronnikovvv@gmail.com>
2023-09-26 22:58:36 +0300

    example to use

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 from requests import get
 
-data = get('http://localhost:9999/api_sst/data/GAZP/2022-05-01/2023-09-26').json()
+data = get('http://127.0.0.1:9999/api_sst/data/GAZP/2022-05-01/2023-09-26').json()
 print(data)
 
-sst_strategies = get('http://localhost:9999/api_sst/strategy/GAZP/2022-05-01/2023-09-26').json()
+sst_strategies = get('http://127.0.0.1:9999/api_sst/strategy/GAZP/2022-05-01/2023-09-26').json()
 print(sst_strategies)
